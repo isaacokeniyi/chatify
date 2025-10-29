35dc4074063124d2b4cb10fb03a58ee0d9328f85
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-10-29 23:18:31 +0000

    implemented auth middleware with routes

--- server/src/routes/messages.route.js
+++ server/src/routes/messages.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { fetchMessages, sendMessages, deleteMessages } from "../controllers/messages.controller.js";
+import authenticateToken from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.get("/messages", fetchMessages);
-router.post("/messages", sendMessages);
-router.delete("/messages/:id", deleteMessages);
+router.post("/messages", authenticateToken, sendMessages);
+router.delete("/messages/:id", authenticateToken, deleteMessages);
 
 export default router;
--- server/src/routes/users.route.js
+++ server/src/routes/users.route.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { deleteUser } from "../controllers/users.controller.js";
+import authenticateToken from "../middleware/auth.middleware.js";
 
 const router = Router();
+
+router.use(authenticateToken);
 
 router.delete("/", deleteUser);
 
