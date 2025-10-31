f5a2bc096bd5eb15b7771fa6fc91e103c4b63d18
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-10-31 23:50:22 +0000

    design for register

--- client/app/auth/register.jsx
+++ client/app/auth/register.jsx
@@ -1,7 +1,65 @@
+import { Link } from "react-router";
+
 export function meta() {
   return [{ title: "Sign up for Chatify" }, { name: "description", content: "The sign up page for chatify" }];
 }
 
-export default function Home() {
-  return <main>Register</main>;
+export default function Register() {
+  return (
+    <main className="w-md p-8 rounded-lg shadow-lg bg-white">
+      <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">SIGN IN</h2>
+      <form className="space-y-4">
+        <div>
+          <label htmlFor="username" className="block text-gray-700">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            placeholder="john doe"
+            className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        <div>
+          <label htmlFor="email" className="block text-gray-700">
+            Email
+          </label>
+          <input
+            type="text"
+            id="email"
+            placeholder="johndoe@example.com"
+            className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        <div>
+          <label htmlFor="password" className="block text-gray-700">
+            Password
+          </label>
+          <input
+            type="text"
+            id="password"
+            placeholder="password"
+            className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        <div className="flex items-center gap-2">
+          <input type="checkbox" id="logMeIn" />
+          <label htmlFor="logMeIn">Log Me In</label>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          REGISTER
+        </button>
+      </form>
+      <p className="mt-4 text-center text-gray-600">
+        Already have an account?{" "}
+        <Link to="/login" className="text-blue-500 hover:text-blue-600 font-semibold">
+          Login here
+        </Link>
+      </p>
+    </main>
+  );
 }
