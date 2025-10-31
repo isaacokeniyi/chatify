b3b2199f4a5a7d01fe4980d2a6bfdb7763ffa12e
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-10-31 23:53:39 +0000

    reroute / to /login

--- client/app/welcome/welcome.jsx
+++ client/app/welcome/welcome.jsx
@@ -1,7 +1,9 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
 export function Welcome() {
-  return (
-    <main className="flex flex-row items-center justify-center px-4 py-4 w-screen h-screen bg-blue-600">
-      <h1 className="text-white font-extrabold text-5xl">WELCOME TO CHATIFY!</h1>
-    </main>
-  );
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/login");
+  }, []);
 }
