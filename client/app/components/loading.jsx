1224aa335345b449f5991aa47d198bd82c50ba9e
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-11-01 21:28:25 +0000

    added loader

---
+++ client/app/components/loading.jsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <p className="text-7xl animate-pulse">Loading chats</p>
+    </div>
+  );
+};
+export default Loading;
