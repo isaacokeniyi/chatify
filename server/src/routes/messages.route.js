6abfa8483b9edc5690ca9aa02e9a0a473baee03a
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-11-01 20:45:35 +0000

    protected all chats route

--- server/src/routes/messages.route.js
+++ server/src/routes/messages.route.js
@@ -4,8 +4,10 @@ import authenticateToken from "../middleware/auth.middleware.js";
 
 const router = Router();
 
+router.use(authenticateToken);
+
 router.get("/messages", fetchMessages);
-router.post("/messages", authenticateToken, sendMessages);
-router.delete("/messages/:id", authenticateToken, deleteMessages);
+router.post("/messages", sendMessages);
+router.delete("/messages/:id", deleteMessages);
 
 export default router;
