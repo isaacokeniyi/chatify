745f07c89a0288b50f73c878aaa82a3c7bbe85b3
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-11-05 22:47:25 +0000

    added colors

--- client/app/chat/chat.jsx
+++ client/app/chat/chat.jsx
@@ -45,23 +45,27 @@ const Chat = () => {
 
   return (
     <main className="h-screen flex flex-col">
-      <div className="h-1/8 flex items-center justify-between px-12">
+      <div className="h-1/8 flex items-center justify-between px-12 bg-[#3b82f6] text-white">
         <p className="text-2xl"> Global Chat</p>
-        <button onClick={handleLogout} className="px-2 py-1 rounded-sm">
+        <button onClick={handleLogout} className="px-2 py-1 rounded-sm bg-red-500 hover:bg-red-600">
           Log out
         </button>
       </div>
-      <div className="h-6/8 flex flex-col items-center gap-4 overflow-y-auto p-4">
+      <div className="h-6/8 flex flex-col items-center gap-4 overflow-y-auto p-4 bg-[#f4f6f8]">
         {messages.map((msg) => (
-          <div key={msg._id} className="max-w-3/5 rounded-sm flex flex-col self-start p-2">
-            <strong>{msg.sender}</strong>
+          <div key={msg._id} className="max-w-3/5 rounded-sm flex flex-col self-start p-2 bg-[#e5e5ea]">
+            <strong className="text-[#9333ea]">{msg.sender}</strong>
             <p className="wrap-break-word pr-4">{msg.message}</p>
           </div>
         ))}
       </div>
       <form className="h-1/8 flex items-center justify-between px-12">
-        <input type="text" placeholder="Write a message" className="w-17/20 h-12 rounded-lg px-4" />
-        <button type="submit" className="px-4 py-2 rounded-md">
+        <input
+          type="text"
+          placeholder="Write a message"
+          className="w-17/20 h-12 rounded-lg px-4 text-[#333333] border border-[#d1d5db]"
+        />
+        <button type="submit" className="px-4 py-2 rounded-md bg-[#9333ea] text-white hover:bg-[#7622c6]">
           SEND
         </button>
       </form>
