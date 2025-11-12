2fbccb05f3def6b44add0dc6c85145c94f320449
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-11-12 06:28:45 +0000

    server sanitization

--- server/src/validators/auth.validator.js
+++ server/src/validators/auth.validator.js
@@ -1,15 +1,26 @@
 import { body } from "express-validator";
+import sanitizeHtml from "sanitize-html";
+
+const sanitizeInput = (input) =>
+  sanitizeHtml(input, {
+    allowedTags: [],
+    allowedAttributes: {},
+  });
 
 export const registerValidator = [
   body("username")
     .trim()
     .matches(/^[a-zA-Z0-9_]{3,16}$/)
-    .withMessage("Username must be 3-16 characters and can only contain letter, numbers or underscores"),
-  body("email").isEmail().withMessage("Invalid Email Address").normalizeEmail(),
-  body("password").isLength({ min: 8 }).withMessage("Password must be at least 8 characters"),
+    .withMessage("Username must be 3-16 characters and can only contain letter, numbers or underscores")
+    .customSanitizer(sanitizeInput),
+  body("email").isEmail().withMessage("Invalid Email Address").normalizeEmail().customSanitizer(sanitizeInput),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters")
+    .customSanitizer(sanitizeInput),
 ];
 
 export const loginValidator = [
-  body("identifier").trim().notEmpty().withMessage("Username or Email is required"),
-  body("password").notEmpty().withMessage("Password is required"),
+  body("identifier").trim().notEmpty().withMessage("Username or Email is required").customSanitizer(sanitizeInput),
+  body("password").notEmpty().withMessage("Password is required").customSanitizer(sanitizeInput),
 ];
--- server/src/validators/messages.validator.js
+++ server/src/validators/messages.validator.js
@@ -1,4 +1,11 @@
 import { body } from "express-validator";
+import sanitizeHtml from "sanitize-html";
+
+const sanitizeInput = (input) =>
+  sanitizeHtml(input, {
+    allowedTags: [],
+    allowedAttributes: {},
+  });
 
 export const messageValidator = [
   body("message")
@@ -6,5 +13,6 @@ export const messageValidator = [
     .notEmpty()
     .withMessage("Message can not be empty")
     .isLength({ max: 500 })
-    .withMessage("Message can not exceed 500 characters"),
+    .withMessage("Message can not exceed 500 characters")
+    .customSanitizer(sanitizeInput),
 ];
