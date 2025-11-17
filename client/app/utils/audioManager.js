0fd70ac4192a8f810e5f709428cc2b191daf044b
Isaac Okeniyi <okeniyiisaac1@gmail.com>
2025-11-17 09:43:50 +0000

    unlock audio

--- client/app/utils/audioManager.js
+++ client/app/utils/audioManager.js
@@ -3,6 +3,18 @@ if (typeof window !== "undefined") {
   sounds = {
     newMessage: new Audio("/app/assets/audio/new.mp3"),
   };
+
+  const unlockAudio = () => {
+    Object.values(sounds).forEach((audio) => {
+      audio.play().catch(() => {});
+    });
+
+    document.removeEventListener("click", unlockAudio);
+    document.removeEventListener("keydown", unlockAudio);
+  };
+
+  document.addEventListener("click", unlockAudio);
+  document.addEventListener("keydown", unlockAudio);
 }
 
 const playSound = (name) => {
